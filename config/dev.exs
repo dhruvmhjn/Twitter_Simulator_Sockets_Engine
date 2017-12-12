138c2fe031cd78fe7d2d9e24c06352cc154ee8e0
Dhruv Mahajan <dhruvmhjn@gmail.com>
2017-12-11 19:12:55 -0500

    d

--- config/dev.exs
+++ config/dev.exs
@@ -42,7 +42,7 @@ config :twitter_engine, TwitterEngineWeb.Endpoint,
   ]
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, level :info, format: "[$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
